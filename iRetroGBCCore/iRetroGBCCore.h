71c1ce97b3e40661e3a27c2588ec6f1b6c6c0fdf
Davide Andreoli <davide.andreoli@icloud.com>
2024-04-29 18:30:42 +0200

    Initial Commit

---
+++ iRetroGBCCore/iRetroGBCCore.h
@@ -0,0 +1,18 @@
+//
+//  iRetroGBCCore.h
+//  iRetroGBCCore
+//
+//  Created by Davide Andreoli on 29/04/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iRetroGBCCore.
+FOUNDATION_EXPORT double iRetroGBCCoreVersionNumber;
+
+//! Project version string for iRetroGBCCore.
+FOUNDATION_EXPORT const unsigned char iRetroGBCCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iRetroGBCCore/PublicHeader.h>
+
+
