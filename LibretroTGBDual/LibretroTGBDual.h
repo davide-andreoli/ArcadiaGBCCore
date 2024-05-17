22e104f6611d5fbe8b0aead2c4f0ca8dbd5783ed
Davide Andreoli <davide.andreoli@icloud.com>
2024-05-17 14:27:01 +0200

    removed submodules

---
+++ LibretroTGBDual/LibretroTGBDual.h
@@ -0,0 +1,18 @@
+//
+//  LibretroTGBDual.h
+//  LibretroTGBDual
+//
+//  Created by Davide Andreoli on 17/05/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LibretroTGBDual.
+FOUNDATION_EXPORT double LibretroTGBDualVersionNumber;
+
+//! Project version string for LibretroTGBDual.
+FOUNDATION_EXPORT const unsigned char LibretroTGBDualVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibretroTGBDual/PublicHeader.h>
+
+
